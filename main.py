325867b4a3914f8fd2d288de73cd7ce323106e61
Gszymon <gladyszymon@student.agh.edu.pl>
2025-12-04 21:09:09 +0100

    Skonczone sprawozdanie

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ import numpy as np
 
 def chebyshev_nodes(n: int = 10) -> np.ndarray | None:
     """Funkcja generująca wektor węzłów Czebyszewa drugiego rodzaju (n,) 
-    i sortująca wynik od najmniejszego do największego węzła.
-
     Args:
         n (int): Liczba węzłów Czebyszewa.
     
@@ -18,7 +16,18 @@ def chebyshev_nodes(n: int = 10) -> np.ndarray | None:
         (np.ndarray): Wektor węzłów Czebyszewa (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    try:
+        if not isinstance(n, int) or n <= 0:
+            return None
+
+        k = np.arange(0, n)
+        x_k = np.zeros(n)
+        for i in range(len(k)):
+            x_k[i] = np.cos(k[i]* np.pi / (n-1))
+        return x_k
+
+    except Exception:
+        return None
 
 
 def bar_cheb_weights(n: int = 10) -> np.ndarray | None:
@@ -31,7 +40,21 @@ def bar_cheb_weights(n: int = 10) -> np.ndarray | None:
         (np.ndarray): Wektor wag dla węzłów Czebyszewa (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    try:
+        if not isinstance(n, int) or n <= 0:
+            return None
+
+        omega_j = np.zeros(n)
+        for j in range(n):
+            if j == 0 or j == n - 1:
+                omega_j[j] = 0.5 * ((-1) ** j)
+            else:
+                omega_j[j] = (-1) ** j
+        return omega_j
+    
+
+    except Exception:
+        return None
 
 
 def barycentric_inte(
@@ -52,7 +75,37 @@ def barycentric_inte(
         (np.ndarray): Wektor wartości funkcji interpolującej (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    try:
+        if not (isinstance(xi, np.ndarray)):
+            return None
+        if not (isinstance(yi, np.ndarray)):
+            return None
+        if not (isinstance(wi, np.ndarray)):
+            return None
+        if not (isinstance(x, np.ndarray)):
+            return None
+        if not (xi.shape == yi.shape == wi.shape):
+            return None
+        
+        n = x.shape[0]
+        m = xi.shape[0]
+        P_x = np.zeros(n)
+        for i in range(n):
+            numerator = 0.0
+            denominator = 0.0
+            for j in range(m):
+                if x[i] == xi[j]:
+                    numerator = yi[j]
+                    denominator = 1.0
+                    break
+                temp = wi[j] / (x[i] - xi[j])
+                numerator += temp * yi[j]
+                denominator += temp
+            P_x[i] = numerator / denominator
+        return P_x
+    
+    except Exception:
+        return None
 
 
 def L_inf(
@@ -71,4 +124,53 @@ def L_inf(
         (float): Wartość normy L-nieskończoność.
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    try:
+        if not (isinstance(xr, (int, float, list, np.ndarray))):
+            return None
+        if not (isinstance(x, (int, float, list, np.ndarray))):
+            return None
+        
+        xr_array = np.array(xr)
+        x_array = np.array(x)
+        if xr_array.shape != x_array.shape:
+            return None
+        return np.max(np.abs(xr_array - x_array))
+    except Exception:
+        return None
+    
+def f_1(x):
+    x = np.asarray(x, dtype=float)
+    sgnx = np.where(x != 0, np.abs(x) / x, 0)
+    return sgnx * x + x**2
+        
+def f_2(x):
+    x = np.asarray(x, dtype=float)  
+    sgnx = np.sign(x)               
+    return sgnx * x**2
+
+def f_3(x):
+    x = np.asarray(x, dtype=float)
+    return np.abs(np.sin(5 * x))**3
+    
+def f_4a(
+            x: float | int 
+            ) -> float | None:
+        try:
+            if not isinstance(x, (int, float)):
+                return None
+            
+            a = [ 1, 25, 100]
+            y = np.zeros(len(a))
+            for i in range(len(a)):
+                y[i] = 1 / (1 + a[i] * x**2)
+            return y    
+
+        except Exception:
+            return None
+        
+def f_5(x):
+    x = np.asarray(x)           # zamieniamy wejście na tablicę (jeśli nie jest)
+    sgnx = np.zeros_like(x)     # domyślnie 0
+    sgnx[x > 0] = 1
+    sgnx[x < 0] = -1
+    return sgnx
